c2c35510cda982047952e462daf35c760b50aff0
Nilay <nnilayy.work@gmail.com>
2025-08-01 04:30:43 +0530

    push

--- pixel.py
+++ pixel.py
@@ -3,3 +3,6 @@ from PIL import Image
 # Create a transparent 1x1 PNG
 img = Image.new("RGBA", (1, 1), (0, 0, 0, 0))  # fully transparent
 img.save("pixel.png", "PNG")
+
+img = Image.new("RGBA", (1, 1), (0, 0, 0, 0))
+img.save("favicon.ico", "ICO")
